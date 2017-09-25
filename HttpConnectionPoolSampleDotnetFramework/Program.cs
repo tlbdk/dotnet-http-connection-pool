f5d8c7eb296806d7fc5f88a2f46ca39d28345bb6
Troels Liebe Bentsen <tlb@rapanden.dk>
2017-09-25 14:31:00 -0700

    Format

--- HttpConnectionPoolSampleDotnetFramework/Program.cs
+++ HttpConnectionPoolSampleDotnetFramework/Program.cs
@@ -9,15 +9,15 @@ namespace HttpConnectionPoolSampleDotnetFramework
 {
     internal class Program
     {
-        
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Started application");
             ConcurrentQueue<string> payloads = new ConcurrentQueue<string>();
-			for (int i = 0; i < 10000; i++)
-			{
-				payloads.Enqueue(i.ToString());
-			}
+            for (int i = 0; i < 10000; i++)
+            {
+                payloads.Enqueue(i.ToString());
+            }
             var test = new ConnectionTest();
             test.StartOldStyle("https://api.staging.connectedcars.io/healthz", 100, payloads);
         }
